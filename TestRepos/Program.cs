52a4bb1eb28b5c196fa15fb6a3fb8d3c2f330f75
bankye <george.williams@gtbank.com>
2018-03-25 17:29:23 +0000

    Add project files.

---
+++ TestRepos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my test Repo App");
+
+            Console.ReadLine();
+        }
+    }
+}
