9337fb52a7eabb5516852a741e5129f86ec2f7a7
bankye <george.williams@gtbank.com>
2018-03-25 18:20:48 +0000

    Test staged

--- TestRepos/Program.cs
+++ TestRepos/Program.cs
@@ -11,6 +11,8 @@ namespace TestRepos
         static void Main(string[] args)
         {
             Console.WriteLine("This is my test Repo App");
+            Console.Write("Change1 done");
+
 
             Console.ReadLine();
         }
